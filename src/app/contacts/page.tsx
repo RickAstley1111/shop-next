68bae0e7a0a41e2609906aa9fa7e4479fe3c291f
Abror <abrorbekburxonov2010@gmail.com>
2025-05-23 14:00:14 +0500

    some changes

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -14,7 +14,9 @@ const page = () => {
                                 inset: 0,
                                 width: '100%',
                                 height: '100%',
-                                border: 0,}}/>
+                                border: 0,
+                            }}
+                        />
                         <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
                             <div className="lg:w-1/2 px-6">
                                 <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
@@ -30,7 +32,7 @@ const page = () => {
                     </div>
                     <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                         <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-                        <p className="leading-relaxed mb-5 text-gray-600">Ozingizni ta'surotizni qoldiring</p>
+                        <p className="leading-relaxed mb-5 text-gray-600">Ozingizni ta&apos;surotizni qoldiring</p>
                         <div className="relative mb-4">
                             <label htmlFor="name" className="leading-7 text-sm text-gray-600">Ism</label>
                             <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
@@ -40,7 +42,7 @@ const page = () => {
                             <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                         </div>
                         <div className="relative mb-4">
-                            <label htmlFor="message" className="leading-7 text-sm text-gray-600">Ta'surot</label>
+                            <label htmlFor="message" className="leading-7 text-sm text-gray-600">Ta&apos;surot</label>
                             <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                         </div>
                         <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Jonatish</button>
--- src/components/FooterniOzi.tsx
+++ src/components/FooterniOzi.tsx
@@ -8,9 +8,9 @@ function FooterniOzi() {
             <div className="container px-5 py-24 mx-auto flex flex-wrap">
                 <div className="flex flex-wrap -mx-4 mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
                     <div className="w-full sm:p-4 px-4 mb-6">
-                        <h1 className="title-font font-medium text-xl mb-2 text-gray-900">O'ziga xos, zamonaviy va ekologik mahsulotlar
-Mahsulotlarimiz zamonaviy dizayn, qulaylik va barqarorlikni birlashtirgan. Siz uchun yaratilgan innovatsion yechimlar.</h1>
-                       
+                        <h1 className="title-font font-medium text-xl mb-2 text-gray-900">
+                            O&apos;ziga xos, zamonaviy va ekologik mahsulotlar. Mahsulotlarimiz zamonaviy dizayn, qulaylik va barqarorlikni birlashtirgan. Siz uchun yaratilgan innovatsion yechimlar.
+                        </h1>
                     </div>
                     <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
                         <h2 className="title-font font-medium text-3xl text-gray-900">2.7K</h2>
@@ -30,7 +30,13 @@ Mahsulotlarimiz zamonaviy dizayn, qulaylik va barqarorlikni birlashtirgan. Siz u
                     </div>
                 </div>
                 <div className="lg:w-1/2 sm:w-1/3 w-full rounded-lg overflow-hidden mt-6 sm:mt-0">
-                    <Image width={600} height={300} className="object-cover object-center w-full h-full" src={FooterImage} alt="stats"/>
+                    <Image
+                        width={600}
+                        height={300}
+                        className="object-cover object-center w-full h-full"
+                        src={FooterImage}
+                        alt="stats"
+                    />
                 </div>
             </div>
         </section>
